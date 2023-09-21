21b55718126afcbe16444f908dd6d12a2adef1be
Pengcheng Zhang <57587456+zpcllyj@users.noreply.github.com>
2023-09-21 16:01:09 -0700

    Changed to Python version

---
+++ SS_Model.py
@@ -0,0 +1,215 @@
+"""
+Python version of the S-S model.
+The S-S model is based on Sobel and Schneider 2009 and 2013 papers, please refer to the papers for more details.
+"""
+
+"""
+This file shows the python code for S-S model.
+"""
+
+import numpy as np
+import xarray as xr
+import matplotlib.pyplot as plt
+
+## ----------------------------------------------
+# model configuration
+DT = 60 # satisfies CFL requirement if wind speed is not insanely large
+TOTAL_INTEGRATION_DAYS = 365*10 # integrate how many years
+SAVEPATH="D:/SS_model_output/" # save path
+# ----------------------------------------------
+
+## ----------------------------------------------
+# Define constants
+BETA=2e-11
+K_V=7786
+EPSILON_U=1e-8
+DELTA_Z=60
+DELTA_Y=50
+T_REF=300
+DELTA=4e3
+GRAVITY=9.81
+HEIGHT=16e3
+TAU=37*24*3600
+Y_ONE=9439e3
+V_D=2.5
+# ----------------------------------------------
+
+
+# ----------------------------------------------
+# define dimensions
+NY = 801
+DY = 15751e3*2/(NY-1)
+Y=np.linspace(start=-15751e3, stop=15751e3,num=NY)
+# ----------------------------------------------
+
+# ----------------------------------------------
+# Define variables u, v and θ. (time, y)
+u=np.zeros([TOTAL_INTEGRATION_DAYS,NY])
+v=np.zeros([TOTAL_INTEGRATION_DAYS,NY])
+theta=np.zeros([TOTAL_INTEGRATION_DAYS,NY])
+time=np.zeros(TOTAL_INTEGRATION_DAYS)
+
+# Define the radiative-convective equilibrium (RCE) temperature θₑ
+theta_00=330
+THETA_E=np.where(np.abs(Y)<Y_ONE,theta_00-DELTA_Y*(Y/Y_ONE)**2,theta_00-DELTA_Y)
+# THETA_E=np.where(np.abs(Y)<Y_ONE,theta_00-DELTA_Y*(np.sin(0.5*np.pi*Y/Y_ONE)**2),theta_00-DELTA_Y)
+
+# ----------------------------------------------
+
+
+# ----------------------------------------------
+# Define necessary Functions
+# Heaviside function: np.heaviside(x1, x2), x2 could be 0.5, i.e., np.heaviside(x, 0.5)
+
+
+# Get potential temperature
+# According to the paper, the factor (pₛ/pₜ)^(R/cₚ) is 1.6
+def get_theta(T):
+    return 1.6*T
+
+
+def get_T(theta):
+    return theta/1.6
+
+# u_grad=np.gradient(theta[0],DY)
+
+
+# First PDE about u:
+def get_dudt(u,v,theta):
+    grad_u=np.gradient(u,DY)
+    grad_v=np.gradient(v,DY)
+    diffusion_u=np.gradient(grad_u,DY)*K_V
+    s=V_D*np.heaviside(u, 0.5)*np.sign(Y)*grad_u
+    f=u*EPSILON_U
+    dudt=v*(BETA*Y-grad_u)-u*grad_v*np.heaviside(grad_v, 0.5)-f-s+diffusion_u
+    return dudt
+
+# Second PDE about v:
+def get_dvdt(u,v,theta):
+    grad_v=np.gradient(v,DY)
+    grad_T=np.gradient(theta/1.6,DY)
+    diffusion_v=np.gradient(grad_v,DY)*K_V
+    return (-BETA*Y*u-GRAVITY*HEIGHT*grad_T/T_REF+diffusion_v)/2
+
+# Third PDE about theta:
+def get_dthetadt(u,v,theta):
+    grad_v=np.gradient(v,DY)
+    return (THETA_E-theta)/TAU-DELTA*DELTA_Z*grad_v/HEIGHT
+
+
+
+# First step: forward Eular
+u_thisstep=np.zeros(NY)
+v_thisstep=np.zeros(NY)
+theta_thisstep=THETA_E
+
+u_before=u_thisstep-DT*get_dudt(u_thisstep,v_thisstep,theta_thisstep)
+v_before=v_thisstep-DT*get_dvdt(u_thisstep,v_thisstep,theta_thisstep)
+theta_before=theta_thisstep-DT*get_dthetadt(u_thisstep,v_thisstep,theta_thisstep)
+
+
+
+total_time_steps=int(86400*TOTAL_INTEGRATION_DAYS/DT) 
+timestamp=0
+day=0
+
+u_temp=np.zeros([int(86400/DT),NY])
+v_temp=np.zeros([int(86400/DT),NY])
+theta_temp=np.zeros([int(86400/DT),NY])
+time_temp=np.zeros(int(86400/DT))
+
+for i in range(total_time_steps):
+    
+
+    # Leap frog
+
+    if i%100==0:
+        v_thisstep[1:-1]=(v_thisstep[:-2]+v_thisstep[2:]+3*v_thisstep[1:-1])/5
+        u_thisstep[1:-1]=(u_thisstep[:-2]+u_thisstep[2:]+3*u_thisstep[1:-1])/5
+
+    u_after=u_before+2*DT*get_dudt(u_thisstep,v_thisstep,theta_thisstep)
+    v_after=v_before+2*DT*get_dvdt(u_thisstep,v_thisstep,theta_thisstep)
+    theta_after=theta_before+2*DT*get_dthetadt(u_thisstep,v_thisstep,theta_thisstep)
+
+    u_before=u_thisstep+0.04*(u_after+u_before-2*u_thisstep)
+    v_before=v_thisstep+0.04*(v_after+v_before-2*v_thisstep)
+    theta_before=theta_thisstep+0.04*(theta_after+theta_before-2*theta_thisstep)
+
+    u_thisstep=u_after
+    v_thisstep=v_after
+    theta_thisstep=theta_after
+
+    u_thisstep[0]=0
+    u_thisstep[-1]=0
+    v_thisstep[0]=0
+    v_thisstep[-1]=0
+
+    timestamp+=DT
+    
+
+    j=(i+1)%int(86400/DT)
+
+    u_temp[j-1]=u_thisstep
+    v_temp[j-1]=v_thisstep
+    theta_temp[j-1]=theta_thisstep
+    time_temp[j-1]=timestamp
+
+    
+    if (i+1)%int(86400/DT)==0:
+        u[day]=np.mean(u_temp,axis=0)
+        v[day]=np.mean(v_temp,axis=0)
+        theta[day]=np.mean(theta_temp,axis=0)
+        time[day]=np.mean(time_temp,axis=0)
+
+        u_temp=np.zeros([int(86400/DT),NY])
+        v_temp=np.zeros([int(86400/DT),NY])
+        theta_temp=np.zeros([int(86400/DT),NY])
+        time_temp=np.zeros(int(86400/DT))
+
+        day+=1
+
+        print(f"Day {day} finished.")
+
+u_xr = xr.DataArray(
+    data=u,
+    dims=["time", "y"],
+    coords={"time": time, "y": Y},
+    attrs=dict(
+        units="m/s",
+    ),
+)
+
+v_xr = xr.DataArray(
+    data=v,
+    dims=["time", "y"],
+    coords={"time": time, "y": Y},
+    attrs=dict(
+        units="m/s",
+    ),
+)
+
+t_xr = xr.DataArray(
+    data=time,
+    dims=["time"],
+    coords={"time": time},
+    attrs=dict(
+        units="seconds since 0000-01-01 00:00:00.0",
+        calendar="noleap"
+    ),
+)
+
+ds=u_xr.to_dataset(name="u")
+ds['v']=v_xr
+ds['time']=t_xr
+
+ds.to_netcdf(SAVEPATH+"test.nc")
+
+fig = plt.figure(figsize=[6,6],dpi=150)
+ax1=fig.add_subplot(1,1,1)
+ax1.plot(np.average(u[-10:],axis=0))
+plt.show()
+
+fig = plt.figure(figsize=[6,6],dpi=150)
+ax1=fig.add_subplot(1,1,1)
+ax1.plot(np.average(v[-10:],axis=0))
+plt.show()
